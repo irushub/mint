a2cb9c0bb2fc197e5f997ffc5fb7cd78f6af93f0
irushub <134095320+irushub@users.noreply.github.com>
2023-11-27 22:54:16 +0530

    Add files via upload

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+
+import React from 'react';
+import MyComponent from './page.tsx';
+
+const HomePage = () => {
+  return (
+    <div>
+      <MyComponent />
+    </div>
+  );
+};
+
+export default HomePage;
